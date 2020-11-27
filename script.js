428e4e5c6046648aada95d2bd552f851a43a59fc
bogdangosa <57357871+bogdangosa@users.noreply.github.com>
2020-11-27 09:35:33 +0200

    Made it more mobile frendly and the links work now

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const Header = document.getElementById("Header");
 window.addEventListener("scroll",()=>{
     let scrolPosition = window.scrollY;
     if(scrolPosition==0)
-        Header.classList.remove("ceva");
+        Header.classList.remove("transparent");
     else
-        Header.classList.add("ceva");
-    console.log(scrolPosition);
+        Header.classList.add("transparent");
+    //sconsole.log(scrolPosition);
 })
